b2efc49016665fc28b3a160686784f27d49b14e8
Tsjippy <ewaldha@hotmail.com>
2026-07-15 18:12:09 +0200

    fixed: block

--- blocks/news-gallery/build/index.asset.php
+++ blocks/news-gallery/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '85dcea307e02baa25572');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '00aa024f00c59192d25f');
