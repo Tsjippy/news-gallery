4a00b9bf5e1e91560fd27d0b544b5e79e308a59a
Tsjippy <ewaldha@hotmail.com>
2026-09-13 06:43:02 +0200

    skip:

--- tsjippy-news-gallery.php
+++ tsjippy-news-gallery.php
@@ -10,7 +10,7 @@ namespace TSJIPPY\NEWSGALLERY;
  * AuthorURI:            harmseninnigeria.nl
  * Requires at least:    6.3
  * Requires PHP:         8.3
- * Plugin URI:            https://github.com/Tsjippy/news-gallery
+ * Plugin URI:           https://github.com/Tsjippy/news-gallery
  * Tested:               7.1
  * TextDomain:           tsjippy
  * Requires Plugins:    
