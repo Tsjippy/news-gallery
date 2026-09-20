9438e3517ef5251b85f78096d1752e225094ca5f
Tsjippy <ewaldha@hotmail.com>
2026-09-20 06:32:50 +0200

    skip:

--- blocks/news-gallery/build/index.asset.php
+++ blocks/news-gallery/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '2b1b4e160f4f3af6d1a0');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'e6cbfe68360bd5eb5861');
